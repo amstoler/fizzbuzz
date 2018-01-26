b20bd064e9497c24aa3a6b9299afbc89654bcd3b
Ariel Stoler <amstoler@gmail.com>
2018-01-26 08:32:47 -0500

    -This is my 7th commit. Start of Bonus. Defined new variables for int multiples and int counters.

--- src/main/java/com/example/fizzbuzz/MainController.java
+++ src/main/java/com/example/fizzbuzz/MainController.java
@@ -8,51 +8,109 @@ import java.util.Scanner;
 @RestController
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping("/bonus")
     public String ShowFizzBuzz() {
 
         //Initializing variables
 
-        int rawFizzResults;
-        int rawBuzzResults;
+        int multiples_7;
+        int multiples_17;
+        int multiples_23;
         int i;
+        int counter_7=0;
+        int counter_17=0;
+        int counter_23=0;
+
         String display = "";
 
-        double userValue; // Stores user number selected for Bonus problem
+        int userValue; // Stores user number selected for Bonus problem
         Scanner keyboard = new Scanner(System.in);
 
-        //Start of my for loop
-
-        userValue = keyboard.nextDouble();
-
+        userValue = keyboard.nextInt();
         System.out.println(userValue);
+
+        //Start of my for loop
 
         for (i = 1; i <= userValue; i++) {
 
 
-            rawBuzzResults = i % 3;
-            rawFizzResults = i % 5;
+            multiples_7 = i % 7;
+            multiples_17 = i % 17;
+            multiples_23 = i % 23;
 
-            if ((rawBuzzResults == 0) && (rawFizzResults == 0)) {
-                System.out.print("FizzBuzz");
-                //String numberFizzBuzz = "Fizzbuzz";
-                display = display + "FizzBuzz";
-            } else if (rawBuzzResults == 0) {
-                System.out.print("Fizz");
-               // String numberFizz = "Fizz";
-                display = display + "Fizz";
-            } else if (rawFizzResults == 0) {
-                System.out.print("Buzz");
-                //String numberBuzz = "Buzz";
-                display = display + "Buzz";
+
+            if (multiples_7 == 0)  {
+                counter_7 = counter_7 +1;
+
+
+            } else if (multiples_17 == 0) {
+                counter_17 = counter_17 +1;
+
+            } else if (multiples_23 == 0) {
+                counter_23 = counter_23 +1;
+
+            }
+            else {
+
+
+            }
+
+        } // End of loop
+        display = "This is the counter for mutiple 7" + counter_7 + "This is...." + counter_17 + "This is..." + counter_23;
+
+
+        return display ; //"numberFizzBuzz" + " " + "numberFizz" + " " + "numberBuzz";
+    }
+
+    @RequestMapping("/bonus")
+    public String ShowFizzBuzz() {
+
+        //Initializing variables
+
+        int multiples_7;
+        int multiples_17;
+        int multiples_23;
+        int i;
+        int counter_7=0;
+        int counter_17=0;
+        int counter_23=0;
+
+        String display = "";
+
+        int userValue; // Stores user number selected for Bonus problem
+        Scanner keyboard = new Scanner(System.in);
+
+        userValue = keyboard.nextInt();
+        System.out.println(userValue);
+
+        //Start of my for loop
+
+        for (i = 1; i <= userValue; i++) {
+
+
+            multiples_7 = i % 7;
+            multiples_17 = i % 17;
+            multiples_23 = i % 23;
+
+
+            if (multiples_7 == 0)  {
+                counter_7 = counter_7 +1;
+
+
+            } else if (multiples_17 == 0) {
+                counter_17 = counter_17 +1;
+
+            } else if (multiples_23 == 0) {
+                counter_23 = counter_23 +1;
+
             }
            else {
 
-                System.out.print(i);
-                display = display + i;
+
                 }
 
         } // End of loop
+            display = "This is the counter for mutiple 7" + counter_7 + "This is...." + counter_17 + "This is..." + counter_23;
 
 
         return display ; //"numberFizzBuzz" + " " + "numberFizz" + " " + "numberBuzz";
